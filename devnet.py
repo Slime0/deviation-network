6a3422492e72afea0afec5bec0a84555d40b63fc
Slime0 <1041839040@qq.com>
2022-09-24 17:30:18 +0800

    完成了代码的阅读和注释

--- devnet.py
+++ devnet.py
@@ -37,6 +37,7 @@ data_format = 0
 
 def dev_network_d(input_shape):
     '''
+    4层深度的模型,输入层(input_shape),隐藏层(1000,2550,20,1)
     deeper network architecture with three hidden layers
     '''
     x_input = Input(shape=input_shape)
@@ -51,6 +52,7 @@ def dev_network_d(input_shape):
 
 def dev_network_s(input_shape):
     '''
+    2层深度的模型,输入层(input_shape),隐藏层(20,1)
     network architecture with one hidden layer
     '''
     x_input = Input(shape=input_shape)
@@ -61,6 +63,7 @@ def dev_network_s(input_shape):
 
 def dev_network_linear(input_shape):
     '''
+    1层直接就是线性模型
     network architecture with no hidden layer, equivalent to linear mapping from
     raw inputs to anomaly scores
     '''    
@@ -70,20 +73,27 @@ def dev_network_linear(input_shape):
 
 def deviation_loss(y_true, y_pred):
     '''
+    loss = (1-y)|dev(x)| + y*max(0,a-dev(x)
     z-score-based deviation loss
     '''    
+
+    # 超参数
     confidence_margin = 5.     
     ## size=5000 is the setting of l in algorithm 1 in the paper
+    # 实例化一个高斯分布的变量，在高斯分布中取5000个，loc是均值，scale是标准差
     ref = K.variable(np.random.normal(loc = 0., scale= 1.0, size = 5000) , dtype='float32')
+    # 是将预测的结果与高斯分布的均值和标准差进行相减
     dev = (y_pred - K.mean(ref)) / K.std(ref)
     inlier_loss = K.abs(dev) 
+
     outlier_loss = K.abs(K.maximum(confidence_margin - dev, 0.))
+    # loss = (1-y)|dev(x)| + y*max(0,a-dev(x))
     return K.mean((1 - y_true) * inlier_loss + y_true * outlier_loss)
 
 
 def deviation_network(input_shape, network_depth):
     '''
-    构建基于偏差网络的检测模型
+    构建基于偏差网络的检测模型,深度分别为4.2,1
     construct the deviation network-based detection model
     '''
     if network_depth == 4:
@@ -94,7 +104,9 @@ def deviation_network(input_shape, network_depth):
         model = dev_network_linear(input_shape)
     else:
         sys.exit("The network depth is not set properly")
+    # 构造一个新的优化器
     rms = RMSprop(clipnorm=1.)
+    # 设定损失函数和指定优化器
     model.compile(loss=deviation_loss, optimizer=rms)
     return model
 
@@ -102,12 +114,15 @@ def deviation_network(input_shape, network_depth):
 def batch_generator_sup(x, outlier_indices, inlier_indices, batch_size, nb_batch, rng):
     """batch generator
     """
+    # 设置随机数种子
     rng = np.random.RandomState(rng.randint(MAX_INT, size = 1))
     counter = 0
     while 1:                
         if data_format == 0:
+            # 获取batch的x和y，其中正常异常各占一半
             ref, training_labels = input_batch_generation_sup(x, outlier_indices, inlier_indices, batch_size, rng)
         else:
+            # 获取batch的x和y，其中正常异常各占一半
             ref, training_labels = input_batch_generation_sup_sparse(x, outlier_indices, inlier_indices, batch_size, rng)
         counter += 1
         yield(ref, training_labels)
@@ -116,15 +131,22 @@ def batch_generator_sup(x, outlier_indices, inlier_indices, batch_size, nb_batch
  
 def input_batch_generation_sup(x_train, outlier_indices, inlier_indices, batch_size, rng):
     '''
+    一个batch中，正常和异常的数量55开
     batchs of samples. This is for csv data.
     Alternates between positive and negative pairs.
     '''      
+    # 获取x的训练集的特征数量
     dim = x_train.shape[1]
+    # 设置一个空的要返回的batch个x的数组
     ref = np.empty((batch_size, dim))    
     training_labels = []
+    # 训练集中正常的总数量
     n_inliers = len(inlier_indices)
+    # 训练集中异常的总数量
     n_outliers = len(outlier_indices)
-    for i in range(batch_size):    
+    for i in range(batch_size):
+        # 从正常和异常的列表中交替取出一条数据，并存入batch
+        # 保证一个batch中，正常和异常的数量是一致的    
         if(i % 2 == 0):
             sid = rng.choice(n_inliers, 1)
             ref[i] = x_train[inlier_indices[sid]]
@@ -133,6 +155,7 @@ def input_batch_generation_sup(x_train, outlier_indices, inlier_indices, batch_s
             sid = rng.choice(n_outliers, 1)
             ref[i] = x_train[outlier_indices[sid]]
             training_labels += [1]
+    # 返回batch的x和y，并且数据类型为np.array
     return np.array(ref), np.array(training_labels)
 
  
@@ -141,11 +164,16 @@ def input_batch_generation_sup_sparse(x_train, outlier_indices, inlier_indices, 
     batchs of samples. This is for libsvm stored sparse data.
     Alternates between positive and negative pairs.
     '''      
+    # 设置一个空的batch_size个的数组
     ref = np.empty((batch_size))    
     training_labels = []
+    # 训练集中正常的总数量
     n_inliers = len(inlier_indices)
+    # 训练集中异常的总数量
     n_outliers = len(outlier_indices)
     for i in range(batch_size):    
+         # 从正常和异常的列表中交替取出一条数据，并存入batch
+        # 保证一个batch中，正常和异常的数量是一致的  
         if(i % 2 == 0):
             sid = rng.choice(n_inliers, 1)
             ref[i] = inlier_indices[sid]
@@ -154,58 +182,85 @@ def input_batch_generation_sup_sparse(x_train, outlier_indices, inlier_indices, 
             sid = rng.choice(n_outliers, 1)
             ref[i] = outlier_indices[sid]
             training_labels += [1]
+    # 转换数据类型为array
     ref = x_train[ref, :].toarray()
     return ref, np.array(training_labels)
 
 
 def load_model_weight_predict(model_name, input_shape, network_depth, x_test):
     '''
+    导入训练好的模型并进行预测
     load the saved weights to make predictions
     '''
+    # 获取预设好的模型结构
     model = deviation_network(input_shape, network_depth)
+    # 导入预设好的模型参数
     model.load_weights(model_name)
+    # 设置模型
     scoring_network = Model(inputs=model.input, outputs=model.output)    
     
+    # 如果是csv数据
     if data_format == 0:
+        # 进行预测
         scores = scoring_network.predict(x_test)
+    # 如果是稀疏矩阵libsvm数据类型
     else:
+        # 获取test数据数量
         data_size = x_test.shape[0]
+        # 创建一个空的分数列表
         scores = np.zeros([data_size, 1])
         count = 512
         i = 0
         while i < data_size:
+            # 分批进行预测，512个数据是一批
             subset = x_test[i:count].toarray()
             scores[i:count] = scoring_network.predict(subset)
             if i % 1024 == 0:
                 print(i)
             i = count
             count += 512
+            # 最后一批如果少于512个，就按照data_size的数量来
             if count > data_size:
                 count = data_size
+        # 如果最后的时候，count != data_size 抛出异常
         assert count == data_size
     return scores
 
 
 def inject_noise_sparse(seed, n_out, random_seed):  
     '''
+    从异常数据中取出n_noise数量的数据,并对其中的5%的特征的数据进行随机交换
     add anomalies to training data to replicate anomaly contaminated data sets.
     we randomly swape 5% features of anomalies to avoid duplicate contaminated anomalies.
     This is for sparse data.
     '''
+    # 设置随机数种子
     rng = np.random.RandomState(random_seed) 
+    # 获取样本数量和特征数量
     n_sample, dim = seed.shape
+    # 随机交换 5% 的异常特征以避免重复污染异常
     swap_ratio = 0.05
+    # 交换特征的数量
     n_swap_feat = int(swap_ratio * dim)
+    # 返回这个矩阵的压缩稀疏列矩阵
     seed = seed.tocsc()
+    # 设置噪音矩阵(压缩稀疏列矩阵)
     noise = csc_matrix((n_out, dim))
     print(noise.shape)
+    
     for i in np.arange(n_out):
+        # 从n_sample中随机选出2条数据的index
         outlier_idx = rng.choice(n_sample, 2, replace = False)
+        # 取出对应的数据
         o1 = seed[outlier_idx[0]]
         o2 = seed[outlier_idx[1]]
+        # 从dim中随机取出5%的异常特征
         swap_feats = rng.choice(dim, n_swap_feat, replace = False)
+        # 将从总的异常数据中，取出的一条异常数据，放进返回的噪音数据列表中
         noise[i] = o1.copy()
+        # 将这条数据中的5%的特征数值和o2的交换
         noise[i, swap_feats] = o2[0, swap_feats]
+    # 返回噪音矩阵的压缩稀疏行矩阵
     return noise.tocsr()
 
 def inject_noise(seed, n_out, random_seed):   
@@ -373,29 +428,46 @@ def run_devnet(args):
             # 重复赋值
             # input_shape = x_train.shape[1:]
             epochs = args.epochs
-            batch_size = args.batch_size    
+            # 得自己通过数据集，计算好下面两个数值
+            # 每一个batch中的数量 
+            batch_size = args.batch_size   
+            # 每一个epochs中的batch的数量 
             nb_batch = args.nb_batch  
             # 取出模型
             model = deviation_network(input_shape, network_depth)
             print(model.summary())  
+            # 设置模型的名字
             model_name = "./model/devnet_"  + filename + "_" + str(args.cont_rate) + "cr_"  + str(args.batch_size) +"bs_" + str(args.known_outliers) + "ko_" + str(network_depth) +"d.h5"
+            # Checkpoint，该回调函数将在每个epoch后保存模型到filepath
+            # monitor：需要监视的值，通常为：val_acc 或 val_loss 或 acc 或 loss
+            # verbose：信息展示模式，0或1。为1表示输出epoch模型保存信息，默认为0表示不输出该信息
+            # save_best_only：当设置为True时，将只保存在验证集上性能最好的模型
+            # save_weights_only：若设置为True，则只保存模型权重，否则将保存整个模型（包括模型结构，配置信息等）
             checkpointer = ModelCheckpoint(model_name, monitor='loss', verbose=0,
                                            save_best_only = True, save_weights_only = True)            
-            
+            # 进行训练
             model.fit_generator(batch_generator_sup(x_train, outlier_indices, inlier_indices, batch_size, nb_batch, rng),
                                           steps_per_epoch = nb_batch,
                                           epochs = epochs,
                                           callbacks=[checkpointer])   
+            # 获取这一轮的训练时间
             train_time += time.time() - start_time
             
-            start_time = time.time() 
+            start_time = time.time()
+            # 进行测试，并返回测试集的分数
             scores = load_model_weight_predict(model_name, input_shape, network_depth, x_test)
+            # 获取这一轮的测试时间
             test_time += time.time() - start_time
+            # 保存每一轮的rauc和ap值(平均准确率)
             rauc[i], ap[i] = aucPerformance(scores, y_test)     
         
+        # auc的平均值
         mean_auc = np.mean(rauc)
+        # auc的方差
         std_auc = np.std(rauc)
+        # ap的平均值(平均准确率)
         mean_aucpr = np.mean(ap)
+        # ap的方差(平均准确率)
         std_aucpr = np.std(ap)
         # 总的训练时间 / 实验重复次数
         train_time = train_time/runs
@@ -403,6 +475,7 @@ def run_devnet(args):
         test_time = test_time/runs
         print("average AUC-ROC: %.4f, average AUC-PR: %.4f" % (mean_auc, mean_aucpr))    
         print("average runtime: %.4f seconds" % (train_time + test_time))
+        # 保存csv格式的结果内容到预设的output路径中去
         writeResults(filename+'_'+str(network_depth), x.shape[0], x.shape[1], n_samples_trn, n_outliers_org, n_outliers,
                      network_depth, mean_auc, mean_aucpr, std_auc, std_aucpr, train_time, test_time, path=args.output)
 
--- utils.py
+++ utils.py
@@ -80,6 +80,9 @@ def get_data_from_svmlight_file(path):
     return data[0], data[1]
 
 def dataLoading(path):
+    """
+    获取数据，label的列名为class
+    """
     # loading data
     df = pd.read_csv(path) 
     
@@ -94,12 +97,17 @@ def dataLoading(path):
 
 
 def aucPerformance(mse, labels):
+    """
+    输入的是测试集的置信度和标签，计算auc和ap
+    """
+    # 使用sklearn中的函数计算auc和ap
     roc_auc = roc_auc_score(labels, mse)
     ap = average_precision_score(labels, mse)
     print("AUC-ROC: %.4f, AUC-PR: %.4f" % (roc_auc, ap))
     return roc_auc, ap;
 
 def writeResults(name, n_samples, dim, n_samples_trn, n_outliers_trn, n_outliers, depth, rauc, ap, std_auc, std_ap, train_time, test_time, path = "./results/auc_performance_cl0.5.csv"):    
+    # 将输入的结果都保存到预设的csv文件中去
     csv_file = open(path, 'a') 
     row = name + "," + str(n_samples)+ ","  + str(dim) + ',' + str(n_samples_trn) + ','+ str(n_outliers_trn) + ','+ str(n_outliers)  + ',' + str(depth)+ "," + str(rauc) +"," + str(std_auc) + "," + str(ap) +"," + str(std_ap)+"," + str(train_time)+"," + str(test_time) + "\n"
     csv_file.write(row)
